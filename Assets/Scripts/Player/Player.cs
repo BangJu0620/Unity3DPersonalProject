b99ac776b2d4187d6ad286ec1258ae07dcdb64b9
BangJu0620 <bju0620@naver.com>
2025-08-11 12:08:07 +0900

    [ chore ] 폰트 설정

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerController controller;
+    public PlayerCondition condition;
+    //public Equipment equip;
+
+    //public ItemData itemData;
+    //public Action addItem;
+
+    //public Transform dropPosition;
+
+    void Awake()
+    {
+        PlayerManager.Instance.Player = this;
+        controller = GetComponent<PlayerController>();
+        condition = GetComponent<PlayerCondition>();
+        //equip = GetComponenet<Equipment>();
+    }
+}
