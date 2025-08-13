0502a7278c3e826df81a741fc76c5238493fb0c5
BangJu0620 <bju0620@naver.com>
2025-08-13 16:23:51 +0900

    [ chore ] 불필요한 부분 수정

--- Assets/Scripts/LaserTrap.cs
+++ Assets/Scripts/LaserTrap.cs
@@ -10,11 +10,6 @@ public class LaserTrap : MonoBehaviour
     public Vector3 origin;
     public TextMeshProUGUI warningText;
 
-    void Start()
-    {
-        
-    }
-
     void Update()
     {
         Debug.DrawRay(transform.position + origin, Vector3.forward, Color.red);
@@ -22,7 +17,6 @@ public class LaserTrap : MonoBehaviour
         if (Physics.Raycast(transform.position + origin, Vector3.forward, distance, playerLayerMask))
         {
             // 경고 띄우기
-            Debug.Log("Player 감지");
             warningText.gameObject.SetActive(true);
         }
         else
